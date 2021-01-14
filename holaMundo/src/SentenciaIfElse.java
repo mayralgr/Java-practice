54919fa58cd966afa0937c47593d4d1154be2c57
mayralgr <ml17ft@gmail.com>
2021-01-13 23:39:28 -0600

    Add if else

---
+++ holaMundo/src/SentenciaIfElse.java
@@ -0,0 +1,10 @@
+public class SentenciaIfElse {
+  public static void main(String[] args) {
+    float promedio = 5.8f;
+    if (promedio >= 6.0f) {
+      System.out.println("Pass");
+    } else {
+      System.out.println("Sad life");
+    }
+  }
+}
